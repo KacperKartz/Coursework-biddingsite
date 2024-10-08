649200ed1ba4dc023edca12b35b54476a1d521cc
Kartzfx <kacperkartz@gmail.com>
2024-10-08 19:06:21 +0100

    Set up database connection and test endpoint

--- server/server.js
+++ server/server.js
@@ -1,12 +1,42 @@
-const express = require("express");
+const express = require('express');
+const { Client } = require('pg');
 const app = express();
+const port = 3000;
+require('dotenv').config();
 
-app.get("/", (req, res) => {
-    res.send("<h1>TEST<h1>")
-})
 
-const port = process.env.PORT || 3000;
 
-app.listen(port, () =>{
-    console.log(`Server running on port ${port}`)
-})
+// Connecting to the database
+const client = new Client({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: String(process.env.PASSWORD),
+  port: process.env.PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+
+ client.connect();
+
+
+
+ //// endpoint for getting users
+ app.get('/users', async (req, res) => {
+    try {
+      const result = await client.query('SELECT * FROM users');
+      res.json(result.rows); // Send the result as JSON
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server Error');
+    }
+  });
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
