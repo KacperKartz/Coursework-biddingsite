04679c2fe3cd33010b783a0e000edea11beb8e49
kivstor10 <94065305+kivstor10@users.noreply.github.com>
2024-10-14 02:41:04 +0100

    added timer component and further developed landing page UI

--- client/src/components/welcome-page.jsx
+++ client/src/components/welcome-page.jsx
@@ -1,21 +1,29 @@
 import welcomeImage from '../assets/Landing Page.png';
 import arrow from '../assets/Arrow.svg';
+import CountdownTimer from './CountdownTimer';
+
+const targetDate = '2024-11-1:59:59';
 
 const WelcomePage = () =>{
     return (
         <>
             <div className="welcome-page-container">
-                <div>
+                <div className="welcome-page-info">
                     <p>
                         Aucto bidding site<br/>2024
                     </p>
+                    <br /><br />
                     <h5>NEW DEALS</h5>
                     <h2>10% off in<br/>November</h2>
+                    <br />
                     <a className='shopNowButton' href='#'>
                         <p>Shop now</p>
                         <img src={ arrow } rel='arrow'/>
                     </a>
-                    <h5>DEAL ENDS IN</h5>
+                    <br /><br /><br /><br />
+                    <h5 className="dealSub">DEAL ENDS IN</h5>
+                    <br />
+                    <CountdownTimer targetDate={targetDate}/>
                 </div>
 
                 <img className="welcomeImage" src={welcomeImage} rel='Welcome Image' />
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -3,13 +3,15 @@ import BentoBox from '../components/BentoBox'
 import Listing from '../components/listing/Listing'
 import Listings from '../components/Listings'
 import WelcomePage from '../components/welcome-page'
+import BackToTopButton from '../components/BackToTopButton'
 
 const LandingPage = () => {
   return (
-    <div className='landingPage'>
+    <div id='landingPage'>
         {/* <BentoBox></BentoBox> */}
         {/* <Listings></Listings> */}
         <WelcomePage />
+        <BackToTopButton />
     </div>
   )
 }
