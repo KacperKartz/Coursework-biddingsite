56917550ebe740b4d0965d7d69ec44fb37b8b1c7
kivstor10 <94065305+kivstor10@users.noreply.github.com>
2024-10-19 18:59:36 +0100

    included react router

--- client/src/components/welcome-page.jsx
+++ client/src/components/welcome-page.jsx
@@ -2,18 +2,21 @@ import welcomeImage from '../assets/Landing Page.png';
 import arrow from '../assets/Arrow.svg';
 import CountdownTimer from './CountdownTimer';
 
-const targetDate = '2024-11-1:59:59';
+const targetDate = '2024-11-03T00:00:00';
 
 const WelcomePage = () =>{
     return (
         <>
             <div className="welcome-page-container">
                 <div className="welcome-page-info">
+                    <div className="vertical-bar"></div>
                     <p>
                         Aucto bidding site<br/>2024
                     </p>
                     <br /><br />
                     <h5>NEW DEALS</h5>
+                    <div className="horizontal-bar bar-black"></div>
+                    <br />
                     <h2>10% off in<br/>November</h2>
                     <br />
                     <a className='shopNowButton' href='#'>
@@ -22,7 +25,7 @@ const WelcomePage = () =>{
                     </a>
                     <br /><br /><br /><br />
                     <h5 className="dealSub">DEAL ENDS IN</h5>
-                    <br />
+                    <div className="horizontal-bar bar-grey"></div>
                     <CountdownTimer targetDate={targetDate}/>
                 </div>
 
