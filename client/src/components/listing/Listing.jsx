3e54c988a346fffe6f96c1216c70cac1b47e1379
kivstor10 <94065305+kivstor10@users.noreply.github.com>
2024-11-08 20:14:21 +0000

    Added initial styling for adding items to watch list

--- client/src/components/listing/Listing.jsx
+++ client/src/components/listing/Listing.jsx
@@ -6,6 +6,8 @@ import Image from './Image';
 import Category from './Category';
 import Rating from './Rating';
 import Button from './Button';
+import WatchITemHeartGrey from '../../assets/watch-item-heart-grey.svg'
+import WatchITemHeartRed from '../../assets/watch-item-heart-red.svg'
 
 const Listing = ({ title, description, price, image, category, rating }) => {
   // Check if product exists and has the necessary fields
@@ -16,6 +18,7 @@ const Listing = ({ title, description, price, image, category, rating }) => {
   return (
     <div className="product-collection-product-listing">
       <div className='image-over'>
+        <img src={WatchITemHeartGrey} rel="Watch Item" className="watch-item-heart"/>
         <Image imageProp={image} className="card-img-top"></Image>
       </div>
 
