7d664fa290a3e3574a9aacb5a9a473c0847e7c90
kivstor10 <94065305+kivstor10@users.noreply.github.com>
2024-11-09 11:30:24 +0000

    Implemented responsive styles targeting iphone screen sizes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,63 +7,73 @@ import login from '../assets/Login.svg';
 import favourites from '../assets/Favourites.svg';
 import basket from '../assets/Basket.svg';
 import search from '../assets/Search.svg';
+import { useState } from 'react';
 
 
 const Navbar = () => {
-  return (
-    <header> 
-        <div className='navbar-container'>
-            <a href="/" style={{ textDecoration: 'none' }}>
-                <img src={ logo } rel='AuctoLogo'/>
-                <h1>Aucto</h1>
-            </a>
-            <ul>
-                <li>
-                    <a href="/" className='activeLink'>
-                        <p>Home</p>
-                        <img src={chevronActive} rel='chevron' />
-                    </a>
-                </li>
-                <li>
-                    <a href="/shop">
-                        <p>Shop</p>
-                        <img src={chevron} rel='chevron' />
-                    </a>
-                </li>
-                <li>
-                    <a href="/#productCategories-container">
-                        <p>Categories</p>
-                        <img src={chevron} rel='chevron' />
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <p>FAQ</p>
-                        <img src={chevron} rel='chevron' />
-                    </a>
-                </li>
-            </ul>
-            <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-                <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"/>
-                <button>
-                    <img src={ search } rel='search icon'/>
-                </button>
-            </form>
-            <div>
-                <button type="button">
-                    <img src={ login } rel='login'/>
-                    <p>Login</p>
-                </button>
-                <a href="#">
-                    <img src={ favourites } rel='chevron' />
-                </a>
-                <a href="#">
-                    <img src={ basket } rel='chevron' />
-                </a>
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);  // Toggle between true/false
+      };
+    return (
+        <header> 
+            <div className={`hamburger-navbar ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <div className="bar1"></div>
+                <div className="bar2"></div>
+                <div className="bar3"></div>
             </div>
-        </div>
-    </header>
-  );
+            <div className={`navbar-container ${isOpen ? 'open' : ''}`}>
+                <a href="/" style={{ textDecoration: 'none' }}>
+                    <img src={ logo } rel='AuctoLogo'/>
+                    <h1>Aucto</h1>
+                </a>
+                <ul>
+                    <li>
+                        <a href="/" className='activeLink'>
+                            <p>Home</p>
+                            <img src={chevronActive} rel='chevron' />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/shop">
+                            <p>Shop</p>
+                            <img src={chevron} rel='chevron' />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/#productCategories-container">
+                            <p>Categories</p>
+                            <img src={chevron} rel='chevron' />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <p>FAQ</p>
+                            <img src={chevron} rel='chevron' />
+                        </a>
+                    </li>
+                </ul>
+                <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+                    <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"/>
+                    <button>
+                        <img src={ search } rel='search icon'/>
+                    </button>
+                </form>
+                <div>
+                    <button type="button">
+                        <img src={ login } rel='login'/>
+                        <p>Login</p>
+                    </button>
+                    <a href="#">
+                        <img src={ favourites } rel='chevron' />
+                    </a>
+                    <a href="#">
+                        <img src={ basket } rel='chevron' />
+                    </a>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Navbar;
