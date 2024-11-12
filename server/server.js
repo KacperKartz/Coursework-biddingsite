0a3ad3da9d8d8daa8df7daf5f75cee64cd9a4ab6
Kartzfx <kacperkartz@gmail.com>
2024-11-12 13:45:32 +0000

    .

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const axios = require('axios');
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = 5000;
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 
